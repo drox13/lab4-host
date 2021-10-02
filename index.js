7a103907327fca7f1dd7d7447f4d3ffa799cac72
drox13 <dario.baron@uptc.edu.co>
2021-10-01 19:34:36 -0500

    ejemplo axios

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 const cors = require('cors');
+const { default: axios } = require('axios');
 var app = express();
 
 const PORT = 9000;
+
+axios.get("/newIntance")
+    .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 
 app.listen(PORT, ()=>{
     console.log(`Server on in port ${PORT}`)
